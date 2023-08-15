2d1152ef5ed20b9b81d1c1a4418e097dfe398ed5
Leanna House <llhouse2@gmail.com>
2023-08-15 16:17:19 -0400

    oops.  Forgot commas in csv files.  haha

--- cropScapeDocumentation/rmWhiteSpFromSubClassFiles.R
+++ cropScapeDocumentation/rmWhiteSpFromSubClassFiles.R
@@ -2,32 +2,33 @@
 setwd("~/Documents/research/imageomics/quest/latLonCover/LatLonCover/cropScapeDocumentation")
 
 
-
-sub<-read.table("CDL_subcategories.csv", sep=",", header=TRUE, stringsAsFactors = FALSE)
+library(rio)
+sub<-import("CDL_subcategories.xlsx", which=3)
 dim(sub)
+head(sub)
 table(sub$courseClass, useNA = "always")
 table(sub$fineClass, useNA = "always")
 
-#uhoh, has white space
+#uhoh, has white space?
 sub$courseClass<-trimws(sub$courseClass, "both")
 sub$fineClass<-trimws(sub$fineClass, "both")
 table(sub$courseClass, useNA = "always")
 table(sub$fineClass, useNA = "always")
 
 
-crsLeg<-read.table("CDL_subcategories_legendCrse.csv", sep=",", header=TRUE, stringsAsFactors = FALSE)
+crsLeg<-import("CDL_subcategories.xlsx", which=4)
 crsLeg$Class<-trimws(crsLeg$Class, "both")
 crsLeg$Definition<-trimws(crsLeg$Definition, "both")
 crsLeg
 
-crsFine<-read.table("CDL_subcategories_legendFine.csv", sep=",", header=TRUE, stringsAsFactors = FALSE)
+crsFine<-import("CDL_subcategories.xlsx", which=5)
 crsFine$Class<-trimws(crsFine$Class, "both")
 crsFine$Definition<-trimws(crsFine$Definition, "both")
 crsFine
 
 ## EXPORT THE CSV FILES
 
-write.table(sub, "CDL_subcategories.csv", quote=FALSE, row.names = FALSE, col.names = TRUE)
-write.table(crsLeg, "CDL_subcategories_legendCrse.csv", quote=FALSE, row.names = FALSE, col.names = TRUE)
-write.table(crsFine, "CDL_subcategories_legendFine.csv", quote=FALSE, row.names = FALSE, col.names = TRUE)
+write.table(sub, "CDL_subcategories.csv", sep=",", quote=FALSE, row.names = FALSE, col.names = TRUE)
+write.table(crsLeg, "CDL_subcategories_legendCrse.csv", sep=",", quote=FALSE, row.names = FALSE, col.names = TRUE)
+write.table(crsFine, "CDL_subcategories_legendFine.csv", sep=",", quote=FALSE, row.names = FALSE, col.names = TRUE)
 
